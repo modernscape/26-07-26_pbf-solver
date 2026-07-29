de183cf7920b5b967a3cf22bc27fbd54b27a5872
aqua <modernscape@gmail.com>
2026-07-29 19:26:18 +0900

    Update target density value in pbfSolver

--- js/pbfSolver.js
+++ js/pbfSolver.js
@@ -5,7 +5,7 @@ export class PbfSolver {
     this.hash = spatialHash
 
     // PBF のパラメータ
-    this.density0 = 800 //6378.0 // 目標密度 (Rest density)
+    this.density0 = 6378.0 //6378.0 // 目標密度 (Rest density)
     this.solverIterations = 2 // 軽快に動かすため、反復回数を2回に最適化
     this.dt = 0.03 // タイムステップ（少し広げて速度感をアップ）
     this.gravity = -9.81 // 重力加速度 (Y軸下向き)
